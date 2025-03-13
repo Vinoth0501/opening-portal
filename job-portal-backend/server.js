51db431b5e49095f0132531d3cfce4143cb644e5
Vinoth kumar <vinofeb23@gmail.com>
2025-03-13 22:35:31 +0530

    Fixed Vercel deployment

--- job-portal-backend/server.js
+++ job-portal-backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://job-portal-002.vercel.app",
+    origin: "https://job-portal-02.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
